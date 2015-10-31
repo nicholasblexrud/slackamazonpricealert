0322f6903de82a3955887dc34d60cd81563b9b08
Nicholas Blexrud <nicholasblexrud@gmail.com>
2015-10-30 22:13:47 -0700

    in the process of cleaning up

--- index.js
+++ index.js
@@ -6,36 +6,61 @@ var schedule = require('node-schedule');
 var SLACK_HOOK_URL = 'https://hooks.slack.com/services/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';
 var toRunEveryHour = '0 * * * *';
 var title = 'Amazing-Meal-Chocolate-36-3-Ounce';
+var url = 'http://www.amazon.com/Amazing-Meal-Chocolate-36-3-Ounce/dp/B0083F5GVW/ref=pd_sim_sbs_325_1?ie=UTF8&refRID=0HMSY3HVXR00PDAGWJEX&dpID=51sc9Xsj2uL&dpSrc=sims&preST=_AC_UL160_SR97%2C160_';
+var priceSelector = '.a-size-large';
+// var slackBotConfigs =         slack.send({
+//             text: title + ': ' + price,
+//             channel: '#amazon',
+//             username: 'Bot',
+//             icon_emoji: 'taco'
+//         });
+
 
 var slack = new Slack(SLACK_HOOK_URL);
 
-schedule.scheduleJob(toRunEveryHour, function () {
-    vo(run)(function (err) {
+
+var AmazonService = {
+    init: function (config) {
+        this.url = config.url;
+        this.selector = config.selector;
+    },
+
+    getPrice: function *() {
+
+        var nightmare = Nightmare();
+
+        var price = yield nightmare
+            .goto(this.url)
+
+            // get price
+            .wait(this.selector)
+            .evaluate(function (sel) {
+                return document.querySelector(sel).innerText;
+            }, this.selector);
+
+        yield nightmare.end();
+
+        return price;
+    },
+
+    handleResults: function (err, res) {
         if (err) {
-            throw err;
+            console.log(err);
         }
-    });
-});
 
-function *run() {
-    var nightmare = Nightmare();
-    var priceSelector = '.a-size-large';
-
-    var price = yield nightmare
-        .goto('http://www.amazon.com/' + title + '/dp/B0083F5GVW/ref=pd_sim_sbs_325_1?ie=UTF8&refRID=0HMSY3HVXR00PDAGWJEX&dpID=51sc9Xsj2uL&dpSrc=sims&preST=_AC_UL160_SR97%2C160_')
-
-        // get price
-        .wait(priceSelector)
-        .evaluate(function (sel) {
-            return document.querySelector(sel).innerText;
-        }, priceSelector)
-
-        slack.send({
-            text: title + ': ' + price,
-            channel: '#amazon',
-            username: 'Bot',
-            icon_emoji: 'taco'
-        });
-
-  yield nightmare.end();
+        console.log(res);
+    }
 }
+
+var config = {
+    url: 'http://www.amazon.com/Amazing-Meal-Chocolate-36-3-Ounce/dp/B0083F5GVW/ref=pd_sim_sbs_325_1?ie=UTF8&refRID=0HMSY3HVXR00PDAGWJEX&dpID=51sc9Xsj2uL&dpSrc=sims&preST=_AC_UL160_SR97%2C160_',
+    selector: '.a-size-large'
+};
+
+
+
+
+
+vo(AmazonService.init, AmazonService.getPrice)(config, AmazonService.handleResults);
+
+
